88eab37cb256bb81e6bca30b6f8f1c5670d8f938
Tom Baranes <tom.baranes@gmail.com>
2016-04-25 19:10:50 +0200

    Add unit tests

--- FittableFontLabel/FittableFontLabel iOS Tests/FittableFontLabelTests.swift
+++ FittableFontLabel/FittableFontLabel iOS Tests/FittableFontLabelTests.swift
@@ -21,16 +21,22 @@ class FittableFontLabelTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+}
+
+extension FittableFontLabelTests {
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    func testMinFontScale() {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 1000, height: 1000))
+        label.text = "Etiam turpis elit, efficitur nec suscipit vitae, aliquam vitae lorem. Aenean mauris tortor, semper vel facilisis nec, facilisis ut neque. Aliquam erat volutpat. Nam eget varius mauris, ut condimentum quam. Duis ex nisi, iaculis in ex ut, lobortis pretium est. Sed non lorem venenatis, fermentum quam aliquet, ultrices arcu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis ullamcorper convallis tellus, vitae bibendum mi dapibus in. Morbi pellentesque aliquet urna, id aliquet orci efficitur sed. Donec et mi urna. Proin sagittis semper massa in semper."
+        label.fontSizeToFit(maxFontSize: 20, minFontScale: 1)
+        XCTAssertEqual(label.font.pointSize, 20)
     }
-    
+
+    func testMaxFontSize() {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 1000, height: 1000))
+        label.text = "Etiam"
+        label.fontSizeToFit(maxFontSize: 20, minFontScale: 0.1)
+        XCTAssertEqual(label.font.pointSize, 20)
+    }
+
 }

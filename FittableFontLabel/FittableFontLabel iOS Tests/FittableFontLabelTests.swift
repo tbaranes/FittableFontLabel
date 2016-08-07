5ecace8969bad4edff7135e53debd6f0af9f0ad3
Tom Baranes <tom.baranes@gmail.com>
2016-08-07 17:31:45 +0200

    Add tests for fontSizeThatFits

--- FittableFontLabel/FittableFontLabel iOS Tests/FittableFontLabelTests.swift
+++ FittableFontLabel/FittableFontLabel iOS Tests/FittableFontLabelTests.swift
@@ -13,11 +13,9 @@ class FittableFontLabelTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
@@ -37,6 +35,15 @@ extension FittableFontLabelTests {
         label.text = "Etiam"
         label.fontSizeToFit(maxFontSize: 20, minFontScale: 0.1)
         XCTAssertEqual(label.font.pointSize, 20)
+    }
+
+    func testMaxFontSizeThatFit() {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 1000, height: 1000))
+        label.text = "Etiam"
+        XCTAssertEqual(20, label.fontSizeThatFits(text: "Etiam", maxFontSize: 20))
+
+        label.text = ""
+        XCTAssertEqual(20, label.fontSizeThatFits(text: "Etiam", maxFontSize: 20))
     }
 
 }
